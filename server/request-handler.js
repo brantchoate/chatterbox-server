7bcb1d586f6b1156e36b299be0864a8ea539ef10
brantchoate <brantchoate@gmail.com>
2015-04-13 17:27:29 -0700

    lkfgsjkgh

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,9 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var payload = {
+  results : []
+}
 
 var requestHandler = module.exports = function(request, response) {
   // Request and Response come from node's http module.
@@ -28,31 +31,41 @@ var requestHandler = module.exports = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
-
-  // The outgoing status.
   var statusCode = 200;
-
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "application/json";
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+  if(request.method === 'GET'){
+
+
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(payload));
+  }
+
+  if(request.method === 'POST'){
+    var str ="";
+    statusCode = 201;
+    response.writeHead(statusCode, headers);
+    request.on('data', function(chunk){
+      str+=chunk;
+    });
+
+   // response.end(JSON.stringify(payload));
+    request.on('end', function() {
+      payload.results.push(str);
+      //get the results and then do JSON.parse
+      response.write(JSON.parse(payload.results));
+
+    });
+
+    response.end('');
+
+  }
+  // The outgoing status.
+
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
